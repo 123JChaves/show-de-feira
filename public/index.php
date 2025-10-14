ccd903b575d428e9da9b4df7c337a962eadb1993
123JChaves <j.blues@hotmail.com>
2025-10-13 23:08:31 -0300

    Adicionados os scripts em .js

--- public/index.php
+++ public/index.php
@@ -13,6 +13,13 @@
     <link rel="stylesheet" href="sweetalert2.min.css">
     <link rel="stylesheet" href="css/style.css">
 
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="sweetalert2.min.js"></script>
+    <script src="js/jquery.inputmask.min.js"></script>
+    <script src="js/jquery.maskedinput-1.2.1.js"></script>
+    <script src="js/parsley.min.js"></script>
+
 </head>
 <body>
     <?php
